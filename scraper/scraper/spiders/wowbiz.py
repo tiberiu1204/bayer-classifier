e4cf21c979b1b51c7d5ca81f92d66700efaa9171
Voicu Ioan-Vladut <77429938+vladvoicux64@users.noreply.github.com>
2024-10-30 19:11:23 +0200

    Refined Wowbiz spider.

--- scraper/scraper/spiders/wowbiz.py
+++ scraper/scraper/spiders/wowbiz.py
@@ -6,38 +6,34 @@ class ArticlesSpider(scrapy.Spider):
 
     def start_requests(self):
         base_url = "https://www.wowbiz.ro"
-        subdirs = ["/exclusiv/", "/video/", "/actualitate/", "/sport/"]
+        subdirs = ["/exclusiv/", "/video/", "/stiri-actualitate/", "/sport/", "/bani/",
+                   "/stiri-economice/", "/stiri-politica/"]
         urls = [base_url + subdir for subdir in subdirs]
 
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse_pages)
 
     def parse_pages(self, response):
-        NUM_PAGES = 5
-        base = ["exclusiv", "video", "actualitate", "sport"]
-        last = response.url.split('/')[-2]
-        if last in base:
-            for page_num in range(2, NUM_PAGES + 1):
-                href = f'{response.url}pagina-{page_num}'
-                yield response.follow(href, callback=self.parse_pages)
-        titles = response.xpath('//article/h2/a')
-        article_arr = []
-        for title in titles:
-            article_arr.append({
-                "href": title.xpath('@href').get(),
-                "text": title.xpath('text()').get()
+        unproc_articles = response.xpath('//*[@id="container"]/div[2]/div/main/div/article')
+        proc_articles = []
+        for article in unproc_articles:
+            proc_articles.append({
+                "href": article.xpath('//a/@href').get(),
+                "text": article.xpath('//a/div[2]/h2/span/text()').get()
             })
 
         # TODO: Filter
 
-        for article in article_arr:
+        for article in proc_articles:
             yield response.follow(article["href"],
                                   callback=self.parse_articles)
-        print(f'Scraped {len(article_arr)} articles from {response.url}')
+        print(f'Scraped {len(proc_articles)} articles from {response.url}')
 
     def parse_articles(self, response):
         yield {
             "text": response.xpath(
-                '//div[contains(@class, "text")]//p//text()'
+                '//*[@id="container"]/div[2]/div/main[2]/div/h2/text() |'
+                '//*[@id="container"]/div[2]/div/main[2]/div/p/text() |'
+                '//*[@id="container"]/div[2]/div/main[2]/div/em/text()'
             ).re(r'\w+')
         }
