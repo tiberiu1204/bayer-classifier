5ea3df36376e01b030c447012af5c313de55f706
Tiberiu Popescu <tiberiu1204@gmail.com>
2024-11-05 20:44:57 +0200

    Calculated per article word frequencies for every website

--- classifier/main.py
+++ classifier/main.py
@@ -17,10 +17,15 @@ CLASSIFIER_DIR = os.getcwd()
 
 
 class Classifier:
+    word_sets = {}
+    word_sets_union = {}
+    freq = {}
+
     def __init__(self):
         self._check_if_files_exist(FILES)
         self.data = self._load_json(FILES)
         self._get_words_sets()
+        self._calculate_frequencies()
     """
     Check if required json files containing necessary data are present in
     current directory.
@@ -36,6 +41,7 @@ class Classifier:
     """
 
     def _check_if_files_exist(self, files: List[str]) -> None:
+        print("[INFO] Fetching jsons...")
         files_not_present = [
             file for file in files if not Path(file).is_file()]
 
@@ -55,6 +61,7 @@ class Classifier:
             thread.join()
 
     def _load_json(self, files: List[str]) -> dict:
+        print("[INFO] Loading jsons...")
         data = {}
         for file in files:
             with open(file, 'r') as f:
@@ -62,15 +69,28 @@ class Classifier:
         return data
 
     def _get_words_sets(self) -> dict:
-        self.words_set = copy.deepcopy(self.data)
-        self.words_set_union = {}
-        for file_name, data_point in self.words_set.items():
+        print("[INFO] Building word sets...")
+        self.word_sets = copy.deepcopy(self.data)
+        self.word_sets_union = {}
+        for file_name, data_point in self.word_sets.items():
             sets = []
             for article in data_point:
                 article["text"] = set(article["text"])
                 sets.append(article["text"])
-            self.words_set_union[file_name] = set.union(*sets)
-        print(self.words_set_union)
+            self.word_sets_union[file_name] = set.union(*sets)
+
+    def _calculate_frequencies(self) -> dict:
+        print("[INFO] Calculating frequencies...")
+        for file_name, article_arr in self.word_sets.items():
+            self.freq[file_name] = Counter()
+            for article in article_arr:
+                self.freq[file_name] = self.freq[file_name] + \
+                    Counter(article["text"])
+            self.freq[file_name] = dict(sorted(
+                ((key, percentage) for key, value in self.freq[file_name].items()
+                 if (percentage := round(float(value) / len(article_arr) * 100, 2))),
+                key=lambda item: item[1], reverse=True
+            ))
 
 
 classifier = Classifier()
